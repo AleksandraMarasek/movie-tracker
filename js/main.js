fdea3feb43fdd82c2e20a38fc4f8ff94ac1f6827
Wer0nikaNiewiad0mska <weronika0426@onet.eu>
2025-12-29 14:41:30 +0100

    working api

--- js/main.js
+++ js/main.js
@@ -1,23 +1,17 @@
-import { searchMovies } from './services/movieService.js';
+import { getHomeMovies } from './services/movieService.js';
 import './components/MovieCard.js';
 
-const form = document.querySelector('.search-form');
-const input = document.querySelector('#search-input');
 const grid = document.getElementById('movies-grid');
 
-form.addEventListener('submit', async (e) => {
-    e.preventDefault();
+(async () => {
     grid.innerHTML = '';
-
-    const movies = await searchMovies(input.value);
+    const movies = await getHomeMovies();
 
     movies.forEach((movie) => {
         const card = document.createElement('movie-card');
-
         card.setAttribute('movie-id', movie.imdbID);
         card.setAttribute('title', movie.Title);
         card.setAttribute('poster', movie.Poster);
-
         grid.appendChild(card);
     });
-});
+})();
--- js/services/movieService.js
+++ js/services/movieService.js
@@ -1,13 +1,24 @@
 import { loadConfig } from '../api/omdb.js';
 
-export async function searchMovies(title) {
+const HOME_IDS = [
+    'tt0111161', //Shawshank
+    'tt0068646', //The Godfather
+    'tt0468569', //The Dark Knight
+    'tt0109830', //Forrest Gump
+    'tt0133093', //Matrix
+    'tt1375666', //Inception
+];
+
+async function getById(imdbID, config) {
+    const url = `${config.OMDB_BASE_URL}?apikey=${config.OMDB_API_KEY}&i=${imdbID}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    if (data.Response === 'False') return null;
+    return data;
+}
+
+export async function getHomeMovies() {
     const config = await loadConfig();
-
-    /*const url = ${config.OMDB_BASE_URL}?apikey=${config.OMDB_API_KEY}&s=${encodeURIComponent(title)}; */
-    const url = 'http://www.omdbapi.com/?i=tt3896198&apikey=5a089b8b';
-    const response = await fetch(url);
-    const data = await response.json();
-
-    if (data.Response === 'False') return [];
-    return data.Search;
+    const movies = await Promise.all(HOME_IDS.map((id) => getById(id, config)));
+    return movies.filter(Boolean);
 }
